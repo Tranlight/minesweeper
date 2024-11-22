ad49b7b1c4464d9ce1c4109a46b631cb224328b9
Tran Ngoc Lai <tranlight98@gmail.com>
2024-11-22 16:10:29 +0700

    Add Validation

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,7 +1,13 @@
 class Board < ApplicationRecord
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :name, :width, :height, :total_mines, presence: true
-  validates :width, :height, :total_mines, numericality: { greater_than: 0 }
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :width, :height, :total_mines, numericality: {
+    only_integer: true, greater_than: 0
+  }
+  validates :width, :height, numericality: {
+    only_integer: true, greater_than: 0, less_than: 2_147_483_647
+  }
   validates :total_mines, numericality: { less_than_or_equal_to: :size }
 
   has_many :mines
